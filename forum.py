278b34920cf0d5c6695fa6d0dca281029cc84e11
MisterKozo <31244997+MisterKozo@users.noreply.github.com>
2018-09-03 23:37:09 +0300

    Add files via upload

--- forum.py
+++ forum.py
@@ -8,15 +8,32 @@ import pickle
 import time
 
 URL_BASE     = "https://www.fxp.co.il/"
+#URL_BASE = "http://127.0.0.1:5000/"
 URL_REGISTER = URL_BASE + "register.php?do=addmember"
+URL_LOGIN    = URL_BASE + "login.php?do=login"
 URL_REPLY    = URL_BASE + "newreply.php?do=postreply&t="
 URL_THREAD   = URL_BASE + "newthread.php?do=postthread&f="
+URL_LIKE     = URL_BASE + "ajax.php?do=add_like"
 
 def string_to_hex(string):
 	return '%'.join("{:02x}".format(ord(c)) for c in string)
+	
+def text_between(s, first, second):
+	try:
+		return s.split(first)[1].split(second)[0]
+	except:
+		return None
+		
+def get_token(ses):
+	res = ses.post(target, headers=header)
+	return res.text.split('<input type="hidden" name="securitytoken" value="')[1].split('"')[0]
 
 def register(ses, email, username, password, useragent):
-	print("Registering\nUser: {0}\nPass: {1}".format(username, password))
+	username = username.replace("\n", "")
+	password = password.replace("\n", "")
+	email = email.replace("\n", "")
+	useragent = useragent.replace("\n", "")
+	print("Registering\nUser: {0}\nPass: {1}\nEmail: {2}".format(username, password, email))
 	password_md5 = hashlib.md5(password).hexdigest()
 	header = {
 		'User-Agent': useragent
@@ -42,7 +59,42 @@ def register(ses, email, username, password, useragent):
 	}
 
 	res = ses.post(URL_REGISTER, headers=header, data=data)
+	#print(res.text)
 	return res if "<strong>{0}</strong>".format(username) in res.text else None
+
+def login(ses, username, password, useragent):
+	username = username.replace("\n", "")
+	password = password.replace("\n", "")
+	useragent = useragent.replace("\n", "")
+	password_md5 = hashlib.md5(password).hexdigest()
+	print("Logging into {0}".format(username))
+	header = {
+		'User-Agent': useragent
+	}
+	data = {
+		#dynamic
+		'vb_login_username': username,
+		'vb_login_password': password,
+		'vb_login_md5password': password_md5,
+		'vb_login_md5password_utf': password_md5,
+		'postvars': '0',
+		'cookieuser': '1',
+		'logintype': '',
+		'cssprefs': '',
+		#hidden
+		's': "",
+		'securitytoken': "guest",
+		'do': "login",
+		'url': URL_BASE,
+	}
+	res = ses.post(URL_LOGIN, headers=header, data=data)
+	userid = text_between(res.text, 'var USER_ID_FXP = "', '"')
+	if userid.isdigit() and int(userid) > 0:
+		url_session = URL_BASE + "?" +text_between(res.text, 'var SESSIONURL = "', '"')
+		#print(ses.post(URL_BASE, headers=header).text)
+		return res
+	else:
+		return None
 	
 def reply(ses, useragent, thread, message):
 	target = URL_REPLY + thread
@@ -51,8 +103,9 @@ def reply(ses, useragent, thread, message):
 	}
 	
 	res = ses.post(target, headers=header)
+	#print(res.text)
 	token = res.text.split('<input type="hidden" name="securitytoken" value="')[1].split('"')[0]
-	userid = res.text.split('var USER_ID_Forum = "')[1].split('"')[0]
+	userid = res.text.split('var USER_ID_FXP = "')[1].split('"')[0]
 	data = {
 		'do': "postreply",
 		's': "",
@@ -66,6 +119,7 @@ def reply(ses, useragent, thread, message):
 		'wysiwyg': "1"
 	}
 	res = ses.post(target, cookies=ses.cookies, headers=header, data=data)
+	#print(res.text)
 	return '<div class="blockrow error" id="qr_error_td"></div>' in res.text
 	
 def thread(ses, useragent, forum, title, message):
@@ -76,7 +130,8 @@ def thread(ses, useragent, forum, title, message):
 	}
 	res = ses.post(target, headers=header)
 	token = res.text.split('<input type="hidden" name="securitytoken" value="')[1].split('"')[0]
-	userid = res.text.split('var USER_ID_Forum = "')[1].split('"')[0]
+	#userid = res.text.split('<input type="hidden" name="loggedinuser" value="')[1].split('"')[0]
+	userid = res.text.split('var USER_ID_FXP = "')[1].split('"')[0]
 	data = {
 		'do': "postthread",
 		's': "",
@@ -93,28 +148,57 @@ def thread(ses, useragent, forum, title, message):
 	res = ses.post(target, headers=header, data=data)
 	return '<div class="blockrow error" id="qr_error_td"></div>' in res.text
 	
+def like(ses, useragent, postid):
+	header = {
+		'User-Agent': useragent,
+		'Origin': URL_BASE[:-1]
+	}
+	res = ses.post("{0}showthread.php?p={1}".format(URL_BASE, postid), headers=header)
+	print(res.text)
+	#cls1 = res.text.split(' onclick="makelike(this.id);"')[0]
+	#cls2 = cls1.split('"')
+	#cls = cls2[-1]
+	#cls = text_between(res.text, '"', ' onclick="makelike(this.id);"')
+	token = res.text.split('<input type="hidden" name="securitytoken" value="')[1].split('"')[0]
+	#userid = res.text.split('<input type="hidden" name="loggedinuser" value="')[1].split('"')[0]
+	userid = res.text.split('var USER_ID_FXP = "')[1].split('"')[0]
+	data = {
+		'do': 'add_like',
+		'postid': str(postid),
+		'fxppro': '',
+		'securitytoken': str(token)
+	}
+	print(data)
+	res = ses.post(URL_LIKE, headers=header, data=data)
+	print("Tried to like "+str(postid))
+	#print(res.text)
+	return res
+	
 class User(object):
-	def __new__(cls, username, email=None):
+	def __new__(cls, username, password=None, email=None):
 		#hi, i want to access username
 		file_path = "users/"+username
 		#check if exists in db, if so, load instance
 		if os.path.isfile(file_path):
 			return pickle.load(open(file_path, 'r'))
-			#return Forum(cookies, email, username, password, useragent)
 		#doesnt exist in db! make sure theres an email
-		if email is None:
+		if email is None and password is None:
 			return None
 		ses = requests.Session()
-		email = email
-		username = username
-		password = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(random.randint(6,12)))
 		useragent = UserAgent().random
-		res = register(ses, email, username, password, useragent)
+		username = username
+		if password is None:
+			password = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) \
+				for _ in range(random.randint(6,12)))
+			res = register(ses, email, username, password, useragent)
+		elif email is None:
+			res = login(ses, username, password, useragent)
 		#if cannot register return none
 		if res is None:
 			return None
 		#didnt return, we have the cookies!
-		userid = res.text.split('var USER_ID_Forum = "')[1].split('"')[0]
+		#print(res.text)
+		userid = res.text.split('var USER_ID_FXP = "')[1].split('"')[0]
 		#create instance
 		ins = Forum(ses, email, username, password, useragent, userid)
 		#dump it
@@ -137,3 +221,6 @@ class Forum(object):
 		
 	def thread(self, forum, title, message):
 		return thread(self.ses, self.useragent, forum, title, message)
+		
+	def like(self, postid):
+			return like(self.ses, self.useragent, postid)
--- master.py
+++ master.py
@@ -1,30 +1,24 @@
+#!/usr/bin/env python
+
 from forum import *
 import sys
 import signal
+import random
+import string
+import time
 
 VERSION = "0.1"
+global user
+user = None
 
 def signal_handler(sig, frame):
 	sys.stdout.write("\nType 'exit' (without apostrophes) to exit\n> ")
         sys.stdout.flush()
 signal.signal(signal.SIGINT, signal_handler)
 
-if len(sys.argv) == 3:
-	user = User(sys.argv[1], sys.argv[2])
-elif len(sys.argv) == 2:
-	user = User(sys.argv[1])
-else:
-	print("Syntax: {0} USERNAME email(if registering)".format(sys.argv[0]))
-	exit()
-
-if user is None:
-	print("Could not login/register. Please try different credentials")
-	exit()
-print("Logged into '{0}' with password '{1}'" \
-	.format(user.username, user.password))
-
 SEPARATOR = "|||"
 def process_command(text):
+	global user
 	com = text.split(SEPARATOR)
 	text_low = com[0].lower()
 	if text_low == "exit":
@@ -32,7 +26,46 @@ def process_command(text):
 	elif text_low == "version":
 		print("You are using version " + VERSION)
 		return True
-	elif text_low == "post":
+	elif text_low == "sleep":
+		if len(com) < 2 or not com[1].isdigit():
+			print("Syntax: sleep|||SECONDS")
+			return False
+		time.sleep(int(com[1]))
+	elif text_low == "register":
+		if len(com) < 2:
+			print("Syntax: register|||USERNAME|||email(optional)")
+			return False
+		username = com[1]
+		if len(com) >= 3:
+			email = com[2]
+		else:
+			email = ''.join(random.choice(string.ascii_lowercase + string.digits) \
+				for _ in range(random.randint(12,14)))+"@gmail.com"
+		user = User(username=username, email=email)
+		if user is None:
+			print("Could not register. Please try different credentials")
+		else:
+			print("Logged into '{0}' with password '{1}'" \
+				.format(user.username, user.password))
+		return True
+	elif text_low == "login":
+		if len(com) < 2:
+			print("Syntax: login|||USERNAME|||password")
+			return False
+		username = com[1]
+		if len(com) >= 3:
+			password = com[2]
+			user = User(username=username, password=password)
+		else:
+			user = User(username=username)
+		if user is None:
+			print("Could not login. Please try different credentials")
+		else:
+			print("Logged into '{0}' with password '{1}'" \
+				.format(user.username, user.password))
+			#print("Session: {0}".format(user.ses.cookies))
+		return True
+	elif text_low == "post" and user is not None:
 		if len(com) != 3:
 			print("Syntax: post|||thread|||content")
 			return False
@@ -45,15 +78,7 @@ def process_command(text):
 		else:
 			print("Error occurred, could not post!")
 		return True
-	elif text_low == "batch":
-		if len(com) != 2:
-			print("Syntax: batch|||FILENAME")
-			return False
-		with open(com[1], 'r') as file_batch:
-			for line in file_batch:
-				process_command(line)
-		return True
-	elif text_low == "thread":
+	elif text_low == "thread" and user is not None:
 		if len(com) != 4:
 			print("Syntax: thread|||num|||subject|||content")
 			return False
@@ -66,10 +91,32 @@ def process_command(text):
 		else:
 			print("Error occurred, could not post!")
 		return True
+	elif text_low == "like" and user is not None:
+		if len(com) != 2:
+			print("Syntax: like|||postid")
+			return False
+		res = user.like(com[1])
+	elif text_low == "batch":
+		if len(com) != 2:
+			print("Syntax: batch|||FILENAME")
+			return False
+		with open(com[1], 'r') as file_batch:
+			for line in file_batch:
+				if line[0] != "#":
+					line = line[:-1]
+					print(">> " + line)
+					process_command(line)
+		return True
 	else:
 		print("Unknown command: '{0}'".format(text_low))
+		if user is None:
+			print("(and/or you're logged out)")
 		return False
 
 while True:
 	text = raw_input("> ")
-	process_command(text)
+	try:
+		process_command(text)
+	except Exception as e:
+		print("Please log-in before executing forum-related commands!")
+		print(str(e))
